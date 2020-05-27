7bdc350cd24d147d89bc1a4d560d04a4327c2a39
miceyjuice <ganjafz@gmail.com>
2020-05-27 19:07:08 +0200

    v0.8

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-let alertBox = document.getElementById("alertMessage");
-let submitButton = document.getElementById("submit");
+const alertBox = document.getElementById("alertMessage");
+const submitButton = document.getElementById("submit");
 
 submitButton.addEventListener("click", function (e) {
     e.preventDefault();
-    let name = document.forms["messageForm"]["yourName"].value;
-    let mail = document.forms["messageForm"]["yourMail"].value;
-    let message = document.forms["messageForm"]["message"].value;
+    const name = document.forms["messageForm"]["yourName"].value;
+    const mail = document.forms["messageForm"]["yourMail"].value;
+    const message = document.forms["messageForm"]["yourMessage"].value;
     if (name === "" || mail === "" || message === "") {
       alertBox.innerHTML="Every field must be filled!";
       return false;
     }else alertBox.innerHTML="";
   }
 )
-
